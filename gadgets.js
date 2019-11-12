bc72f28d5db704fb4e05db53397da598965dce65
ward46 <56239097+ward46@users.noreply.github.com>
2019-11-11 20:20:28 -0600

    Delete gadgets.js

--- gadgets.js
+++
@@ -1,9 +0,0 @@
-var Mongoose = require('mongoose');
-var Schema = Mongoose.Schema;
-
-var GadgetsSchema = new Schema({    
-    Yoo: { type: String, requred: true },    
-    Hoo: { type: Number, requred: true, default: 10 }
-});
-
-module.exports =  Mongoose.model('Gadgets', GadgetsSchema);
